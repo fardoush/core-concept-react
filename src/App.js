316a27d2952c15f9714689995416deca579b142b
fardoush <fardoushjahanara@gmail.com>
2023-03-21 20:53:28 +0600

    api fetch

--- src/App.js
+++ src/App.js
@@ -1,25 +1,82 @@
-import logo from './logo.svg';
 import './App.css';
 
+import { useEffect, useState } from 'react';
+
 function App() {
+  const products = [
+    {name:'Laptop', price: 153000},
+    {name: 'Phone', price: 93000},
+    {name: 'Watch', price:33000},
+    {name: 'Table', price:5000}
+  ]
   return (
+    // <div className="App">
+    //   {products.map(product => <Product name={product.name} price={product.price}></Product>)}
+      
+    //  {/* <Product name="Laptop" price="94000"></Product>
+    //  <Product name="Phone" price="24000"></Product>
+    //  <Product name="Watch" price="2400"></Product>
+    //  <Product name="Tablet" price="31000"></Product> */}
+    // </div>
+
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      {/* <Counter> </Counter> */}
+      <ExternalUsers> </ExternalUsers>
+      
     </div>
   );
 }
 
+
+function ExternalUsers(){
+  const [users, setUsers] = useState([]);
+  useEffect(()=>{
+fetch('https://jsonplaceholder.typicode.com/users')
+.then(res => res.json()).then(data => console.log(data))
+
+    
+  }, [])
+   
+  return(
+    <div className="">
+      <h2 className="">External Users</h2>
+    </div>
+  )
+}
+
+
+// function Product(props){
+//   return(
+// <div className="product">
+//   <h3 className="">Name: {props.name} </h3>
+//   <p className="">Price: {props.price}</p>
+// </div>
+
+
+//   )
+// }
+
+function Counter(){
+  const [count, setCount] = useState(0);
+
+  const increaseCount = () => setCount(count + 1);
+  const decreaseCount = () => setCount(count - 1);
+  // const increaseCount = () =>{
+  //   const newCount = count + 1;
+  //   setCount(newCount);
+  // }
+  // const abc = useState(30);
+  // console.log(abc);
+  
+  return(
+
+    <div className="">
+      <h1 className="">Count: {count} </h1>
+      <button onClick={increaseCount}>Increase</button>
+      <button onClick={decreaseCount}>Decrease</button>
+    </div>
+
+  )
+}
 export default App;
